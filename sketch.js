322370d62c6a1793e520c0fc0cb00ee0132f650b
Shijie Zhou <cs5129606@gmail.com>
2020-01-02 00:45:26 -0700

    adding some text

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ function setup() {
     createCanvas(displayWidth, displayHeight);
     strokeWeight(1);
     stroke(0);
+    textSize(12);
+    text('Draw here', width/2, 100);
+    fill(0, 102, 153);
 }
 
 function touchMoved() {
